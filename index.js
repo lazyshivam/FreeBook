b461f66a71f699868ab86c2a80c0eb728f6dfcdb
lazyshivam <shivamgoswami.ss.pp@gmail.com [filter lfs]>
2022-12-22 11:19:49 +0530

    some changes

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const connectToMongo= require('./db')
 const express = require('express')
+const cors=require('cors');
 
  connectToMongo()
 
 const app = express()
+
 const port = 4000
 
 app.use(express.json());
+app.use(cors());
 //end point for routes
 app.use('/api/auth',require('./routes/auth'));
 app.use('/api/notes',require('./routes/notes'));
